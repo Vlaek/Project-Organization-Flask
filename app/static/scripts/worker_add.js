475b4d7772464dedb72c52b5ef1a873fcbb06919
Vlaek <vlad.eichwald@gmail.com>
2023-07-08 01:31:06 +0700

    7.0

--- app/static/scripts/worker_add.js
+++ app/static/scripts/worker_add.js
@@ -1,80 +1,66 @@
-let modal1 = document.getElementById('id01');
 
-window.onclick = function(event) {
-    if (event.target == modal1) {
-        modal1.style.display = "none";
-    };
 
-    cancel.onclick = function() {
-        modal1.style.display = "none";
-    };
+//
+//function OnChange() {
+//    if (forename.value == '' || surname.value == '' || dob.value == '') {
+//        document.getElementById("btn1").setAttribute("disabled", "disabled");
+//        console.log("da")
+//    } else {
+//        document.getElementById("btn1").removeAttribute("disabled");
+//    }
+//}
+//
+//OnChange();
+//
 
-    kreuz.onclick = function() {
-        modal1.style.display = "none";
-    };
+const forename = document.getElementById("forename");
+const surname = document.getElementById("surname");
+const dob = document.getElementById("DOB");
+const speciality = document.getElementById("speciality");
 
-    function getRandomInRange(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
+const btnAdd = document.getElementById('add');
+const modal1 = document.getElementById('modal1');
+const btnClose1 = document.getElementById('btn-close1');
+const randomBtn = document.getElementById('random');
+
+btnAdd.addEventListener('click', () => {
+    modal1.style.display = 'flex';
+})
+
+btnClose1.addEventListener('click', () => {
+    modal1.style.display = 'none';
+})
+
+window.addEventListener('click', (e) => {
+    const target = e.target;
+
+    if (!target.closest('.modal__content') && !target.closest('.header__btn')) {
+        modal1.style.display = 'none';
     }
+})
 
-    function formatDate(year, month, day) {
-
-      if (day < 10) day = '0' + day;
-
-      if (month < 10) month = '0' + month;
-
-      return year + '-' + month + '-' + day;
-    }
-
-    random.onclick = function(){
-        $( document ).ready(function(){
-            var url = "https://api.randomdatatools.ru/?count=10&params=LastName,FirstName";
-            $.getJSON( url, {
-                format: "json"
-            })
-            .done(function( data ) {
-                document.getElementById('forename').value = data[1]["FirstName"];
-                document.getElementById('surname').value = data[1]["LastName"];
-                document.getElementById('DOB').value = formatDate(getRandomInRange(1950, 2002), getRandomInRange(1, 12), getRandomInRange(1,28));
-                let o1 = getRandomInRange(0, 4)
-                if (o1 == 0)
-                {
-                    document.querySelector('#speciality').value = "Инженер";
-                }
-                if (o1 == 1)
-                {
-                    document.querySelector('#speciality').value = "Техник";
-                }
-                if (o1 == 2)
-                {
-                    document.querySelector('#speciality').value = "Конструктор";
-                }
-                if (o1 == 3)
-                {
-                    document.querySelector('#speciality').value = "Лаборант";
-                }
-                if (o1 == 4)
-                {
-                    document.querySelector('#speciality').value = "Обслуживающий персонал";
-                }
-
-                OnChange();
-            });
+randomBtn.addEventListener('click', () => {
+    const specialities = ['Инженер', 'Техник', 'Конструктор', 'Лаборант', 'Обслуживающий персонал'];
+    $(document).ready(() => {
+        let url = 'https://api.randomdatatools.ru/?count=10&params=LastName,FirstName';
+        $.getJSON(url, {
+            format: 'json'
+        })
+        .done((data) => {
+            forename.value = data[1]['FirstName'];
+            surname.value = data[1]["LastName"];
+            dob.value = formatDate(getRandomInRange(1950, 2002), getRandomInRange(1, 12), getRandomInRange(1, 28));
+            speciality.value = specialities[getRandomInRange(0, 4)];
         });
-    };
+    });
+});
+
+function getRandomInRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-const forename = document.getElementById("forename").value;
-const surname = document.getElementById("surname").value;
-const dob = document.getElementById("DOB").value;
-
-function OnChange() {
-    if (forename.value == '' || surname.value == '' || dob.value == '') {
-        document.getElementById("btn1").setAttribute("disabled", "disabled");
-        console.log("da")
-    } else {
-        document.getElementById("btn1").removeAttribute("disabled");
-    }
+function formatDate(year, month, day) {
+  if (day < 10) day = '0' + day;
+  if (month < 10) month = '0' + month;
+  return year + '-' + month + '-' + day;
 }
-
-OnChange();
