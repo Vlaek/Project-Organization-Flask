aab3bae6c7ccb07aaf30290fa73a6adf1b61af96
Vlaek <vlad.eichwald@gmail.com>
2023-07-11 00:55:07 +0700

    11.0

--- app/static/scripts/subcontractor_edit.js
+++
@@ -1,8 +0,0 @@
-function modal_data(id, name, cost, name_project, workload) {
-    let a = document.getElementById("id02").style.display='block';
-    document.getElementById("idSubcontractor2").value = id;
-    document.getElementById("Name2").value = name;
-    document.getElementById("Cost2").value = cost;
-    document.getElementById("NameProject2").value = name_project;
-    document.getElementById("Workload2").value = workload;
-};
